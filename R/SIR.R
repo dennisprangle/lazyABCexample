fa656f48060a9f5fb44dbd1e8ac48f169c0af940
Dennis Prangle <dennis.prangle@gmail.com>
2014-09-15 11:16:49 +0100

    Allow output thinning

--- R/SIR.R
+++ R/SIR.R
@@ -14,16 +14,21 @@
 #' @param norm_beta Whether beta should be normalised (i.e. divided) by population size
 #' @param t_end Time at which the simulation will terminate
 #' @param step_end Number of steps after which the simulation will terminate
-#' @return A matrix with S, I, R, t (these 4 are the model output) and elapsed (computer time taken) columns is returned.  This includes the initial state.
+#' @param thinning An integer determining what type of output to return. Zero gives the final state. A positive value thins the output by this factor i.e. \code{output=1} gives all output, \code{output=2} gives results for step 0,2,4,... The final state is always returned.
+#' @return If \code{thinning>0} a matrix is returned with columns step (number of steps performed), S, I, R (state at this step) and elapsed (computer time taken). The steps reported depend on the thinning variable. If \code{thinning==0} then only the final step is returned, as a vector.
 
 #' @export
-SIRsim <- function(beta, gamma, S0, I0, R0, t0, norm_beta=TRUE, t_end=Inf, step_end=Inf) {
+SIRsim <- function(beta, gamma, S0, I0, R0, t0, norm_beta=TRUE, t_end=Inf, step_end=Inf, thinning=0) {
     start_time <- proc.time()[3]
     if (norm_beta) beta <- beta / (S0+I0+R0)
     ##Initialise output
-    output <- matrix(nrow=2*S0+I0+1, ncol=5) ##Memory allocated for max number of events plus first state
-    colnames(output) <- c("S","I","R","t","elapsed")
-    output[1,] <- c(S0,I0,R0,t0,0)
+    if (thinning>0) {
+        output <- matrix(nrow=ceiling((2*S0+I0+1)/thinning), ncol=6) ##n.b. 2*S0+I0+1 is maximum number of events, and 1 is added for initial state
+        colnames(output) <- c("step","S","I","R","t","elapsed")
+        output[1,] <- c(0,S0,I0,R0,t0,0)
+        thincounter <- 0 ##How many steps since output last recorded
+        recordedsofar <- 1 ##How many outputs recorded so far
+    }
     ##Initialise current state
     S <- S0; I <- I0; R <- R0; t <- t0; step <- 0
     ##Main loop
@@ -41,10 +46,27 @@ SIRsim <- function(beta, gamma, S0, I0, R0, t0, norm_beta=TRUE, t_end=Inf, step_
             I <- I-1; R <- R+1
         }       
         step <- step+1
-        output[step+1,] <- c(S,I,R,t,proc.time()[3]-start_time)
+        if (thinning > 0) {
+            thincounter <- thincounter+1
+            if (thincounter == thinning) {
+                output[recordedsofar+1,] <- c(step,S,I,R,t,proc.time()[3]-start_time)
+                recordedsofar <- recordedsofar+1
+                thincounter <- 0
+            }
+        }
         if (step==step_end || I==0) break;
     }
-    return(output[seq(1,step+1),])
+    if (thinning==0) {
+        output <- c(step,S,I,R,t,proc.time()[3]-start_time)
+        names(output) <- c("step","S","I","R","t","elapsed")
+    } else {
+        if (thincounter>1) {
+            output[recordedsofar+1,] <- c(step,S,I,R,t,proc.time()[3]-start_time)
+            recordedsofar <- recordedsofar+1
+        }        
+        output <- output[seq(1,recordedsofar),,drop=FALSE]
+    }
+    return(output)
 }
 
 #' @export
