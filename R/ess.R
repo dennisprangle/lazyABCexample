3290a19aed49257adeec974071e5cfddffc92295
Dennis Prangle <dennis.prangle@gmail.com>
2014-09-18 09:27:36 +0100

    Add ess function

---
+++ R/ess.R
@@ -0,0 +1,11 @@
+#' Calculate ESS
+#'
+#' Calculates effective sample size from a vector of weights
+#'
+#' @param w Vector of weights
+#'
+#' @return Effective sample size
+#' @export
+ess <- function(w) {
+    mean(w^2) / mean(w)^2
+}
